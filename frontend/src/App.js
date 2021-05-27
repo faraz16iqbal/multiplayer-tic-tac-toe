7eb16848b609b23393abd36231be4a6c6f64ecf9
Faraz <faraziqbal@hotmail.co.uk>
2021-05-27 06:19:47 +0530

    Configured frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,85 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { socket } from "./utils/socket";
-import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import GameScreen from "./screens/GameScreen";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
-  const [yourID, setYourID] = useState(null);
-  const [message, setMessage] = useState(null);
-  const [room, setRoom] = useState("");
-  const [name, setName] = useState("");
-  // const [msg, setMsg] = useState("");
-
-  useEffect(() => {
-    socket.on("connected", (data) => {
-      setYourID(data.id);
-    });
-
-    socket.on("message", (m) => {
-      setMessage(m);
-    });
-
-    socket.on("newGameCreated", (temp) => {
-      console.log(temp);
-      setRoom(temp);
-    });
-    // socket.on("newRoomJoin", (data) => {
-    //   console.log(data);
-    //   // setRoom(temp);
-    // });
-    socket.on("waiting", () => {
-      console.log("WAITING");
-      // setRoom(temp);
-    });
-    socket.on("pieceAssignment", (data) => {
-      console.log(data);
-      // setRoom(temp);
-    });
-    socket.on("starting", (data) => {
-      console.log(data);
-    });
-  }, []);
-
-  const joinRoom = (e) => {
-    e.preventDefault();
-    const roomObj = {
-      name,
-      room,
-    };
-    setMessage(name);
-    setRoom("");
-    setName("");
-    socket.emit("newRoomJoin", roomObj);
-  };
-
-  const newRoom = (e) => {
-    e.preventDefault();
-    socket.emit("newGame");
-  };
-
-  return (
-    <div className="App">
-      <form onSubmit={joinRoom}>
-        <input
-          type="text"
-          placeholder="Enter Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Enter code to join room"
-          value={room}
-          onChange={(e) => setRoom(e.target.value)}
-        />
-        <button type="submit">Join Room</button>
-      </form>
-      <button onClick={newRoom} type="submit">
-        New Room
-      </button>
-
-      <h1> {message && message}</h1>
-    </div>
-  );
+  <Router>
+    <Route path="/game" exactscomponent={GameScreen} />
+    <Route path="/" exact component={HomeScreen} />
+  </Router>;
 };
 
 export default App;
