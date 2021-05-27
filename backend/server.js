4e39a55f2bf055a4e58e16d06e501ae2bcad7538
Faraz <faraziqbal@hotmail.co.uk>
2021-05-27 06:10:23 +0530

    Fixed Cors Issue and Updated Socket.io

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,12 @@ import cors from "cors";
 import Player from "./utils/player.js";
 const app = require("express")();
 const httpServer = require("http").createServer(app);
-const io = require("socket.io")(httpServer);
+
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
 
 import {
   rooms,
@@ -106,6 +111,7 @@ io.on("connection", (socket) => {
   });
 
   socket.on("disconnecting", () => {
+    console.log("User disconnected");
     //Get all the rooms that the socket is currently subscribed to
     const currentRooms = Object.keys(socket.rooms);
     //In this game an object can only have 2 rooms max so we check for that
@@ -130,7 +136,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 httpServer.listen(PORT, () =>
   console.log(`Server running on : http://localhost:${PORT}`)
--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,4 +1,4 @@
 import io from "socket.io-client";
-const SOCKET_URL = "http://localhost:5001";
+const SOCKET_URL = "http://localhost:5000";
 
 export const socket = io(SOCKET_URL);
