14196fb574d31db827e3344a59cca7a7d4a9639d
Faraz <faraziqbal@hotmail.co.uk>
2021-05-29 01:11:31 +0530

    Added socket url

--- frontend/src/config/socket.js
+++ frontend/src/config/socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
-const SOCKET_URL = process.env.REACT_APP_SOCKET_URL;
+const SOCKET_URL = "https://faraz-tic-tac-toe.herokuapp.com";
 
 console.log(SOCKET_URL);
 export const socket = io(SOCKET_URL, {
