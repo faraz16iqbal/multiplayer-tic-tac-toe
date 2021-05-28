85fd4a0634ff1111d7b53a78cef0b6612b27bb20
Faraz <faraziqbal@hotmail.co.uk>
2021-05-29 00:07:33 +0530

    Changes

--- frontend/src/config/socket.js
+++ frontend/src/config/socket.js
@@ -1,6 +1,7 @@
 import io from "socket.io-client";
-const SOCKET_URL = process.env.SOCKET_URL;
+const SOCKET_URL = process.env.REACT_APP_SOCKET_URL;
 
+console.log(SOCKET_URL);
 export const socket = io(SOCKET_URL, {
   withCredentials: true,
 });
