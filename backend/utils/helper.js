8b8390d24842c806c384f782dca7fb5b56282cb1
Faraz <faraziqbal@hotmail.co.uk>
2021-05-29 02:09:06 +0530

    Changed UUID function

--- backend/utils/helper.js
+++ backend/utils/helper.js
@@ -1,7 +1,8 @@
 const { v4 } = require("uuid");
 
+// randomly generate 5 characters
 const genRoom = () => {
-  return v4().replaceAll("-", "").slice(0, 15).toUpperCase();
+  return v4().replaceAll("-", "").slice(0, 5).toUpperCase();
   // return "12345";
 };
 
