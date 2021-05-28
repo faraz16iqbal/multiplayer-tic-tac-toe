b7c054fa351b02bf59058bd272c3b941410b3ed8
Faraz <faraziqbal@hotmail.co.uk>
2021-05-29 02:51:48 +0530

    Heroku fix

--- backend/utils/helper.js
+++ backend/utils/helper.js
@@ -1,8 +1,8 @@
-const { v4 } = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 
 // generate unquie id
 const genRoom = () => {
-  return v4().replaceAll("-", "").slice(0, 5).toUpperCase();
+  return uuidv4().replaceAll("-", "").slice(0, 5).toUpperCase();
 };
 
 const randomPiece = () => {
