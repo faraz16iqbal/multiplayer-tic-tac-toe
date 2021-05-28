c2c6c87fffcdb9f881706b2e53008b40766a7dec
Faraz <faraziqbal@hotmail.co.uk>
2021-05-29 02:58:52 +0530

    Fixed UUID

--- backend/utils/helper.js
+++ backend/utils/helper.js
@@ -1,8 +1,10 @@
-const { v4: uuidv4 } = require("uuid");
-
 // generate unquie id
 const genRoom = () => {
-  return uuidv4().replaceAll("-", "").slice(0, 5).toUpperCase();
+  return "xxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16).toUpperCase();
+  });
 };
 
 const randomPiece = () => {
